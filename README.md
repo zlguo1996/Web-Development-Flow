ffd4e06f9c789f8e614aa373c6ad5d7c380849da
Guo Zile <guozile@corp.netease.com>
2022-09-22 15:43:38 +0800

    fix: code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# test-vscode-extension README
+# web-development-flow README
 
-This is the README for your extension "test-vscode-extension". After writing up a brief description, we recommend including the following sections.
+This is the README for your extension "web-development-flow". After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,55 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from "child_process";
+import { GitExtension } from './git';
 
-let diagnosticCollection: vscode.DiagnosticCollection;
+function getGitRepository() {
+	const gitAPI = vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports?.getAPI(1);
+	if (!gitAPI) { throw new Error('Extension vscode.git should be enabled'); };
+	const rootPath = vscode.workspace.workspaceFolders?.[0]?.uri;
+	if (!rootPath) { throw new Error('rootPath not exists'); };
+	const repository = gitAPI.getRepository(rootPath);
+	if (!repository) { throw new Error(`repository not exists for path - ${rootPath}`); };
+	return repository;
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const disposables = [
+		vscode.commands.registerCommand('web-development-flow.helloWorld', () => {
+			vscode.window.showWarningMessage('Hello World from web-development-flow!');
+		}),
+		vscode.commands.registerCommand('web-development-flow.copyAsLink', () => {
+			const repository = getGitRepository();
+			const activeTextEditor = vscode.window.activeTextEditor;
+			if (activeTextEditor) {
+				const start = activeTextEditor.selections?.[0]?.start?.line;
+				const end = activeTextEditor.selections?.[0]?.end?.line;
+				const info = {
+					line: start === end ? `${start + 1}` : `${start + 1}-${end + 1}`,
+					path: vscode.workspace.asRelativePath(activeTextEditor.document.fileName),
+					commit: repository.state.HEAD?.commit,
+					remote: repository.state.remotes?.[0]?.fetchUrl
+				};
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test-vscode-extension" is now active!');
+				const url = `${info.remote?.replace(/\.git$/, '/-/blob/')}${info.commit}/${info.path}#L${info.line}`;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-vscode-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showWarningMessage('Hello World from test-vscode-extension!');
+				vscode.env.clipboard.writeText(url);
+			}
+		}),
+		vscode.commands.registerCommand('web-development-flow.openWithTypora', () => {
+			const activeTextEditor = vscode.window.activeTextEditor;
+			if (activeTextEditor) {
+				cp.exec(`open -a typora ${activeTextEditor.document.fileName}`);
+			}
+		})
+	];
+
+	disposables.forEach((disposable) => {
+		context.subscriptions.push(disposable);
 	});
-
-	context.subscriptions.push(disposable);
-
-	ctx.subscriptions.push(getDisposable());
-  diagnosticCollection = vscode.languages.createDiagnosticCollection('go');
-  ctx.subscriptions.push(diagnosticCollection);
 }
 
 // this method is called when your extension is deactivated
