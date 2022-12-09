1c9fc4d8e29defde5206cd30a211503cab811002
Guo Zile <guozile@corp.netease.com>
2022-12-09 15:26:32 +0800

    feat: syncFromMaster

---
+++ src/command/syncFromMaster.ts
@@ -0,0 +1,44 @@
+/**
+ * Fetch origin master branch and merge into current branch
+ */
+
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import { getGitRepository } from "./util";
+
+export default () => vscode.window.withProgress({
+    location: vscode.ProgressLocation.Notification,
+    title: 'Sync From Master'
+}, syncFromMaster);
+
+/**
+ * 
+ * @param targetRepositoryName targetRepository should be in same folder of current repository
+ */
+async function syncFromMaster(progress: vscode.Progress<{ message?: string; increment?: number }>) {
+    try {
+        const repository = await getGitRepository();
+        const branchName = repository.state.HEAD?.name;
+        if (!branchName) { throw new Error('Failed to get current branch name'); };
+        const defaultBranchName = 'master'; // NOTE default branch may not be master, fix later
+        await repository.checkout(defaultBranchName);
+        progress.report({ message: `Checkout ${defaultBranchName} success` });
+        await repository.pull();
+        progress.report({ message: `Pull ${defaultBranchName} success` });
+        await repository.checkout(branchName);
+        try {
+            await (repository as any)?.repository?.merge(defaultBranchName);
+        } catch (e: any) {
+            console.error(e);
+            throw new Error(`Merge ${defaultBranchName}: ${e.message}(${e.gitArgs?.join(' ')}) \n ${e.stderr}`);
+        }
+        progress.report({ message: `Merge ${defaultBranchName} success` });
+    } catch (e: any) {
+        console.error(e);
+        vscode.window.showErrorMessage(e.message);
+    }
+}
+
+export function remote2RepositoryPrefix(remote: string) {
+    return remote.replace(/.+\/\/[^\/]+\/(.+)\.git/, '$1');
+}
--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@ import * as vscode from 'vscode';
 import * as cp from "child_process";
 import copyAsLink from './command/copyAsLink';
 import mergeIntoFork from './command/mergeIntoFork';
+import syncFromMaster from './command/syncFromMaster';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -24,7 +25,8 @@ export function activate(context: vscode.ExtensionContext) {
 				cp.exec(`open -a typora ${activeTextEditor.document.fileName}`);
 			}
 		}),
-		vscode.commands.registerCommand('web-development-flow.mergeIntoFork', mergeIntoFork)
+		vscode.commands.registerCommand('web-development-flow.mergeIntoFork', mergeIntoFork),
+		vscode.commands.registerCommand('web-development-flow.syncFromMaster', syncFromMaster),
 	];
 
 	disposables.forEach((disposable) => {
