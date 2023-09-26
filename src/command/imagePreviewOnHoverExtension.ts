7184731d2bee0b765648ff6d746d0e7701d2a23f
zlguo1996 <zlguo1996@gmail.com>
2023-09-26 10:36:00 +0800

    feat: test <img> tag

--- src/command/imagePreviewOnHoverExtension.ts
+++ src/command/imagePreviewOnHoverExtension.ts
@@ -14,12 +14,17 @@ export default function activateImagePreviewOnHoverExtension(context: vscode.Ext
                     const urlRegex = /(https?:\/\/)*[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
                     const range = document.getWordRangeAtPosition(position, urlRegex);
                     if (range) {
-                        const url = document.getText(range);
+                        const imageUrl = document.getText(range);
                         const imageSuffixRegex = /\.(png|jpg|jpeg|gif|webp|svg|ico|bmp|tiff|tif|jfif|ppm|pgm|pbm|pnm|ppm|xbm|xbm|xpm|xwd|x3f|x3m|x3a|x3e|x3g|x3b|x3d|x3f|x3m|x3a|x3e|x3g|x3b|x3d|x3f|x3m|x3a|x3e|x3g|x3b|x3d|x3f|x3m|x3a|x3e|x3g|x3b|x3d|x3f|x3m|x3a|x3e|x3g|x3b|x3d|x3f|x3m|x3a|x3e|x3g|x3b|x3d|x3f|x3m|x3a|x3e|x3g|x3b|x3d|x3f|x3m|x3a|x3e|x3g|x3b|x3d|x3f|x3m|x3a|x3e|x3g|x3b|x3d|x3f|x3m|x3a|x3e|x3g|x3b|x3d)$/i;
-                        const isImage = imageSuffixRegex.test(url);
+                        const isImage = imageSuffixRegex.test(imageUrl);
                         if (isImage) {
-                            const markdownString = new vscode.MarkdownString(`![image](${url})`);
+                            const markdownString = new vscode.MarkdownString(`![image](${imageUrl})`);
                             return new vscode.Hover(markdownString);
+
+                            // const markdownString = new vscode.MarkdownString();
+                            // markdownString.appendMarkdown(`<div class="image-container"><img src="${imageUrl}"></div>`);
+                            // markdownString.appendMarkdown(`<style>.image-container{max-width:100%;max-height:100%;overflow:auto;}.image-container img{max-width:100%;max-height:100%;object-fit:contain;}</style>`);
+                            // return new vscode.Hover(markdownString);
                         }
                     }
                 }
